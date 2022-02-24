f0081dee8b0b804cd39f65a9b0ac07892b6fc206
sumeetvaidya <esumeet@gmail.com>
2022-02-23 22:48:21 -0500

    fixed boarding_app.py file

--- poc/sumeet/apps/boarding_pass.py
+++ poc/sumeet/apps/boarding_pass.py
@@ -11,7 +11,7 @@ def app():
     st.title('Boarding Pass')
 
     st.write("This is a sample page in the mutliapp.")
-    st.write("See `token/boarding_pass.py` to know how to use it.")
+    st.write("See `app/boarding_pass.py` to know how to use it.")
 
     st.markdown("### Sample Data")
 
