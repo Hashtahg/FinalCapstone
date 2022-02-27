bf1881531aa3358f2b48ad951872dbb9eec829c0
Patrick Beeson <patrickbeeson@Patricks-MBP.fios-router.home>
2022-02-27 12:57:03 -0500

    def app issue

---
+++ multi_app_poc/sumeet/app.py
@@ -0,0 +1,18 @@
+#=======================
+#Streamlit main app
+#=======================
+
+import streamlit as st
+from multiapp import MultiApp
+
+#import your app modules here
+from apps import biometric_login, boarding_pass 
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Biometric Login", biometric_login.app)
+app.add_app("Boarding Pass", boarding_pass.app)
+
+# The main app
+app.run()
